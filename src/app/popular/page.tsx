f78ba661f3b84d93c7d07a0f7402a1387537c735
osnaider <osnaidermartinez2002@gmail.com>
2023-12-02 23:02:40 -0500

    add pagination

--- src/app/popular/page.tsx
+++ src/app/popular/page.tsx
@@ -1,63 +1,44 @@
-import { Card } from "@/components/card/Card";
-import { FilterGenresTags } from "@/components/filters/FilterGenresTags";
 import { FiltersAll } from "@/components/filters/FiltersAll";
 import { Search } from "@/components/search/Search";
-import { queryAnimePopular } from "@/querys/query";
-import { Pagination } from "@/types/types";
 import { Suspense } from "react";
+import { Animes } from "./AnimePagination";
+import { SkeletonCards } from "@/components/skeleton/SkeletonCards";
 
-async function getAnimePopular() {
-  const { query, variables } = queryAnimePopular;
-  try {
-    const response = await fetch("https://graphql.anilist.co", {
-      cache: "force-cache",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        query,
-        variables,
-      }),
-    });
-    if (!response.ok) {
-      throw new Error("error  al obtener  los datos");
-    }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
-}
-export default async function PopularPage() {
-  const data: Pagination = await getAnimePopular();
-  console.log(data);
+type PageParams = {
+  searchParams: {
+    genres?: string;
+    page?: number;
+    search?: string;
+    year?: string;
+    season?: string;
+    seasonyear?: string;
+    format?: string;
+    sort: string;
+  };
+};
+export default async function PopularPage({ searchParams }: PageParams) {
+  const page = searchParams.page;
+  const genres = searchParams.genres;
+  const search = searchParams.search;
+  const sort = searchParams.sort;
   return (
     <div className="pt-[70px] relative w-full h-full">
       <div className="w-[80%] m-auto h-full">
         <div className="w-full h-full flex items-center justify-between py-5">
           <h1 className="text-3xl font-semibold">Most Popular Anime</h1>
-          <Search/>
+          <Search />
         </div>
         <div>
           <Suspense fallback={<div>loader</div>}>
             <FiltersAll />
           </Suspense>
         </div>
-        <div className="h-full w-full ">
-          <div className="flex  gap-x-2 gap-y-4 items-center flex-wrap  justify-between py-6">
-            {data.data.Page.media.map((anime) => (
-              <Card
-                key={anime.id}
-                id={anime.id}
-                color={anime.coverImage.color}
-                genres={anime.genres}
-                imagen={anime.coverImage.large}
-                title={anime.title.userPreferred}
-              />
-            ))}
-          </div>
-        </div>
+        <Suspense
+          key={page + genres + search + sort}
+          fallback={<SkeletonCards />}
+        >
+          <Animes searchParams={searchParams} />
+        </Suspense>
       </div>
     </div>
   );
