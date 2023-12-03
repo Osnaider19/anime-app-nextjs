af2eea9b143a974c0c481c941e178f9a02f81644
osnaider <osnaidermartinez2002@gmail.com>
2023-12-02 23:02:05 -0500

    add search

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,10 +1,34 @@
 "use client";
 import { Input } from "@nextui-org/react";
-
+import { useSearchParams } from "next/navigation";
+import { usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
 export const Search = () => {
+  const pathname = usePathname();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const params = new URLSearchParams(searchParams);
+  const handelChange = useDebouncedCallback((query: string) => {
+    if (query) {
+      params.set("search", query);
+    } else {
+      params.delete("search");
+    }
+    params.delete("page");
+    router.replace(`${pathname}?${params}`);
+  }, 500);
   return (
-    <div className="flex w-[200px]  rounded-2xl overflow-hidden">
-      <Input type="text" placeholder="search" size="sm" />
+    <div className="flex w-[200px]">
+      <Input
+        type="text"
+        placeholder="search"
+        size="sm"
+        onChange={(e) => handelChange(e.target.value)}
+        defaultValue={params.get("search")?.toString()}
+        variant="bordered"
+        radius="full"
+      />
     </div>
   );
 };
