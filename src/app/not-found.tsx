6ebc3184cf07adb716f4174b83cc421121b346a2
osnaider <osnaidermartinez2002@gmail.com>
2023-12-04 18:39:45 -0500

    add page not found

---
+++ src/app/not-found.tsx
@@ -0,0 +1,23 @@
+import { Button } from "@nextui-org/react";
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="h-screen w-full flex justify-center items-center flex-col pt-[60px]">
+      <div className="w-[50%] h-[90%] relative">
+        <img
+          src="../../404-2.webp"
+          alt=""
+          className="absolute left-0 top-0 w-full h-full object-cover"
+          loading="lazy"
+        />
+      </div>
+      <div className="py-1 w-full  flex justify-center items-center flex-col gap-1">
+        <p className="text-2xl font-semibold ">404 - Page Not Found</p>
+        <Button href="/" as={Link} color="secondary" variant="solid">
+          Return Home
+        </Button>
+      </div>
+    </div>
+  );
+}
