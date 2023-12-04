c2988af69ab44915caa5e490280f30b39e4223fe
osnaider <osnaidermartinez2002@gmail.com>
2023-12-03 22:21:21 -0500

    add new card hover decription

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -6,11 +6,53 @@ type Props = {
   title: string;
   genres: string[];
   color: string;
+  description?: string;
+  episodes?: number;
+  averageScore?: number;
+  yearStart?: number;
+  yearEnd?: number;
+  hover?: boolean;
 };
-export const Card = ({ id, imagen, title, genres, color }: Props) => {
+export const Card = ({
+  id,
+  imagen,
+  title,
+  genres,
+  color,
+  description,
+  episodes,
+  averageScore,
+  yearEnd,
+  yearStart,
+  hover,
+}: Props) => {
   return (
     <Link href={`/anime/${id}`}>
-      <div className="w-[200px] h-[250px] relative border border-[#ffffff20] overflow-hidden rounded-md shadow-2xl shadow-[#ffffff10]">
+      <div className="w-[200px]  h-[270px] relative border border-[#ffffff20] overflow-hidden rounded-lg shadow-2xl shadow-[#ffffff10] group">
+        {/* {hover && (
+          <div className="absolute left-0 top-0 w-full h-full opacity-0 transition-opacity duration-300 pointer-events-none bg-black/95 z-10 group-hover:opacity-100 group-hover:pointer-events-auto px-2 py-3">
+            <div className="w-full h-full flex  flex-col ">
+              <div className="flex items-center py-1 gap-x-1">
+                {episodes && <p className="text-sm">{episodes} episodes</p>}
+                <IconsPoint />
+                <p className="text-sm ">
+                  {yearStart !== yearEnd && yearEnd && yearStart
+                    ? `${yearStart} - ${yearEnd}`
+                    : yearStart}
+                </p>
+              </div>
+              {averageScore && <span>{`${averageScore}%`}</span>}
+              {description && (
+                <div
+                  className="w-full overflow-hidden text-sm line-clamp-[9]"
+                  dangerouslySetInnerHTML={{
+                    __html: description,
+                  }}
+                ></div>
+              )}
+            </div>
+          </div>
+        )} */}
         <img
           src={imagen}
           alt={`imagen de ${title}`}
