2aa6b73a8efe79e07c902aeed2f8d2f5260d21de
osnaider <osnaidermartinez2002@gmail.com>
2024-03-08 17:45:30 -0500

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { lato } from "@/fonts/fonts";
 import NextTopLoader from "nextjs-toploader";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Footer } from "@/components/footer/Footer";
+import { Analytics } from "@vercel/analytics/react";
 export const metadata: Metadata = {
   title: "NexAnime",
   description: `Follow, discover and share the most popular and best-rated anime through reviews and recommendations. everything here in Nexanime`,
@@ -15,6 +16,7 @@ export default function RootLayout(props: { children?: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={lato.className} suppressHydrationWarning={true}>
+        <Analytics />
         <Providers>
           <div className="w-full h-full relative">
             <div className="relative top-0 left-0 w-full h-full dark dark:bg-[#000] dark:text-white    max-w-[1355px] mx-auto">
